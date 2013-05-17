9098ba80c5c45c24de56c04e217699d3ed7d3d6c
Quentin Decock <quentin.decock@gmail.com>
2013-05-17 14:42:07 -0400

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Monocle::Application.config.secret_token = '94d15afd5b93325670acae0d9c7e5f3f30778d0620e972e60814520f2fd82db530e7a223e2938f298fb04ed83564ee9f0e85bc418db0190816be6a047ccb6de9'
