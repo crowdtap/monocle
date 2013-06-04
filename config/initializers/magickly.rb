8af81dee89dd71b73a5143e24a64f81e35655e55
Quentin Decock <quentin.decock@gmail.com>
2013-06-04 15:09:54 -0400

    adds magickly

---
+++ config/initializers/magickly.rb
@@ -0,0 +1,15 @@
+Magickly.dragonfly.configure do |c|
+  # c.log_commands = true
+
+  c.analyser.add :face_data do |temp_object|
+    Mustachio.face_data(temp_object)
+  end
+
+  c.analyser.add :face_data_as_px do |temp_object, width, height|
+    Mustachio.face_data_as_px(temp_object, width, height)
+  end
+
+  c.analyser.add :face_span do |temp_object|
+    Mustachio.face_span(temp_object)
+  end
+end
