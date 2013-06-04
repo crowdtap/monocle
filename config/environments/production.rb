1e23a6c85287431939d56730272a2250e3fc7445
Quentin Decock <quentin.decock@gmail.com>
2013-06-04 15:18:59 -0400

    disabling Rack::Cache

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,9 @@ Monocle::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  # Rack Cache
+  config.action_dispatch.rack_cache = false
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
