cc164541ca9d343114f9922ea5c13c88e3385cc9
Robert Stern <roberts@crowdtap.com>
2014-03-25 11:11:10 -0400

    Align for (arjun)

--- spec/integration/monocle_spec.rb
+++ spec/integration/monocle_spec.rb
@@ -11,8 +11,8 @@ describe Monocle do
     it "returns 404 if the image doesn't exist" do
       stub_request(:get, 'http://www.example.com/images/logo.jpg')
         .to_return(
-           :status => 404,
-           :body => '',
+           :status  => 404,
+           :body    => '',
            :headers => {}
         )
       get '/transform_image/q/src/http%3A%2F%2Fwww.example.com%2Fimages%2Flogo.jpg/optimized_thumb/200x100'
@@ -22,8 +22,8 @@ describe Monocle do
     it "returns 200 if the image exists" do
       stub_request(:get, 'http://www.example.com/images/logo.jpg')
         .to_return(
-           :status => 200,
-           :body => File.read('spec/fixtures/crowdtap-logo.jpg'),
+           :status  => 200,
+           :body    => File.read('spec/fixtures/crowdtap-logo.jpg'),
            :headers => {}
         )
       get '/transform_image/q/src/http%3A%2F%2Fwww.example.com%2Fimages%2Flogo.jpg/optimized_thumb/200x100'
