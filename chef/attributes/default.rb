1f9866dad4bd798df19d9aa67bd12643799e9512
Michael D'Auria <michael.dauria@gmail.com>
2014-09-02 16:59:29 -0400

    Working setup

--- chef/attributes/default.rb
+++ chef/attributes/default.rb
@@ -1,1 +1,13 @@
-override[:application] = 'monocle'
+default[:application] = 'monocle'
+default[:rubies] = ['2.1.2-github1']
+
+default[:fiance][:url] = 'http://fiance.vpc.crowdtap.com'
+default[:monocle][:url] = '//dgj5ep7xp9u24.cloudfront.net'
+default[:promiscuous][:amqp_url] = 'amqp://crowdtap:OpTDZCTyVnOqAhOgISXuKiTnjgHGsGYRmOrQWDlpJyXonh@rabbit.vpc.crowdtap.com:5672'
+default[:promiscuous][:rabbit_mgmt_url] = 'http://crowdtap:OpTDZCTyVnOqAhOgISXuKiTnjgHGsGYRmOrQWDlpJyXonh@rabbit.vpc.crowdtap.com:15672'
+default[:promiscuous][:redis_url] = 'redis://promiscuous.sxc8di.0001.use1.cache.amazonaws.com:6379'
+default[:redis][:url] = 'redis://sidekiq.sxc8di.0001.use1.cache.amazonaws.com:6379'
+default[:sidekiq][:redis_url] = 'redis://sidekiq.sxc8di.0001.use1.cache.amazonaws.com:6379'
+default[:sniper][:url] = 'http://sniper.vpc.crowdtap.com/'
+default[:sniper][:notification_url] = 'http://sniper-bg.vpc.crowdtap.com/'
+default[:waldo][:url] = 'http://waldo.vpc.crowdtap.com'
