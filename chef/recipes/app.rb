23776bc8951c32f2f4ed75278e2fb7bf26c5fb73
Michael D'Auria <michael.dauria@gmail.com>
2014-09-17 14:29:53 -0400

    Now with Chef!
    Adds all of the details required to run a monocles server in the production
    environment (and virtualbox too).

---
+++ chef/recipes/app.rb
@@ -0,0 +1,15 @@
+package 'advancecomp'
+package 'gifsicle'
+package 'jhead'
+package 'jpegoptim'
+package 'libjpeg-progs'
+package 'optipng'
+package 'pngquant'
+
+crowdtap_ruby_application "monocle" do
+  ruby_version '2.1.2-github1'
+  unicorns
+  nginx do
+    template 'etc/nginx/sites-available/monocle.erb'
+  end
+end
---
+++ chef/recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: monocle
+# Recipe:: default
+#
+# Copyright (C) 2014 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe 'monocle::app'
