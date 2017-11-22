0c8f89329a219e175aaf30321d40ccaa6650797e
Matthias Johnson <opennomad@gmail.com>
2017-11-22 09:29:21 -0700

    trying 8 worker procs

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -18,7 +18,7 @@ else
   # Use at least one worker per core if you're on a dedicated server,
   # more will usually help for _short_ waits on databases/caches.
   #worker_processes 8
-  worker_processes 3
+  worker_processes 8
   listen 80
   logger Logger.new(STDOUT)
 
